bc42900444eb68060ab229414c13f049380f29de
Cookie <cookieplug@gmail.com>
2021-12-13 21:57:33 +1100

    Added comments.

--- getRootHash.js
+++ getRootHash.js
@@ -6,4 +6,7 @@ const hashedAddresses = addresses.map(addr => keccak256(addr));
 const merkleTree = new MerkleTree(hashedAddresses, keccak256, { sortPairs: true });
 const rootHash = merkleTree.getRoot().toString('hex');
 
+// Note, you should only include the FINAL root hash in your Solidity smart contract
+// after no further changes need to be made to the `addresses.json` file.
+
 console.log(`Root Hash: 0x${rootHash}`);
